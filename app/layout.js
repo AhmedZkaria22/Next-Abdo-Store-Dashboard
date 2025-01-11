cca17def0ceb5acffc18c7638ee4d3a9ad028c65
AhmedZkaria22 <ahmedzkaria372017@gmail.com>
2025-01-12 01:57:41 +0200

    handle multi meta layout

--- app/layout.js
+++ app/layout.js
@@ -9,7 +9,14 @@ export const metadata = {
       description: "Abdo store dashboard related to abdo store ecommerce website, to handle website statistics , products and customers",
       keywords: "Abdo store dashboard, Abdo store ecommerce, Abdo store website, Dashboard, Website, Ecommerce, Abdo store, Nextjs, Reactjs, Tailwind, Tailwindcss, React-bootstrap",
       type: 'website',
-      image: images.dashboard,
+      // image: './images/dashboard.png',      
+      // twitterImage: './images/dashboard.png',      
+      images: '@/app/images/dashboard.png',      
+      twitterImages: '@/app/images/dashboard.png',      
+      // images: images.dashboard,      
+      // twitterImages: images.dashboard,      
+      // image: 'https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D',      
+      // twitterImage: 'https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D',      
     }
   
 };
